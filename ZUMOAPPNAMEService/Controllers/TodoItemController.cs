57201cfc1e89b829bf2ae1d7a6c276a0cb0a5326
Brenton Reddish <brentonr13@gmail.com>
2020-08-16 22:34:33 +1000

    Add project files.

---
+++ ZUMOAPPNAMEService/Controllers/TodoItemController.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.OData;
+using Microsoft.Azure.Mobile.Server;
+using ZUMOAPPNAMEService.DataObjects;
+using ZUMOAPPNAMEService.Models;
+
+namespace ZUMOAPPNAMEService.Controllers
+{
+    public class TodoItemController : TableController<Assets>
+    {
+        protected override void Initialize(HttpControllerContext controllerContext)
+        {
+            base.Initialize(controllerContext);
+            ZUMOAPPNAMEContext context = new ZUMOAPPNAMEContext();
+            DomainManager = new EntityDomainManager<Assets>(context, Request);
+        }
+
+        // GET tables/TodoItem
+        public IQueryable<Assets> GetAllTodoItems()
+        {
+            return Query();
+        }
+
+        // GET tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public SingleResult<Assets> GetTodoItem(string id)
+        {
+            return Lookup(id);
+        }
+
+        // PATCH tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task<Assets> PatchTodoItem(string id, Delta<Assets> patch)
+        {
+            return UpdateAsync(id, patch);
+        }
+
+        // POST tables/TodoItem
+        public async Task<IHttpActionResult> PostTodoItem(Assets item)
+        {
+            Assets current = await InsertAsync(item);
+            return CreatedAtRoute("Tables", new { id = current.Id }, current);
+        }
+
+        // DELETE tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task DeleteTodoItem(string id)
+        {
+            return DeleteAsync(id);
+        }
+    }
+}
---
+++ ZUMOAPPNAMEService/DataObjects/Assets.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Azure.Mobile.Server;
+using System;
+
+namespace ZUMOAPPNAMEService.DataObjects
+{
+    public class Assets : EntityData
+    {
+        public string substation_code { get; set; }
+
+        public string Name { get; set; }
+        public string plant_number { get; set; }
+
+        public int asset_eq_no { get; set; }
+
+        public string eq_status { get; set; }
+
+        public string serial_number { get; set; }
+
+        public string modifier_code { get; set; }
+
+        public int location_equipment_number { get; set; }
+
+        public string component_code { get; set; }
+
+        public DateTime warrantydate { get; set; }
+
+        public int equipment_age { get; set; }// needs auto timer
+
+        public string stock_code { get; set; }
+
+        public string po_no { get; set; }
+
+        public int rated_volts { get; set; }
+
+        public int nominal_volts { get; set; }
+
+        public string manufacture_name { get; set; }
+
+        public string specifiaction_title { get; set; }
+
+        public string specification_no { get; set; }
+
+        public string specifiaction_item_no { get; set; }
+
+        public string last_install_date { get; set; }
+
+        public string equipment_class { get; set; }
+
+        public string Equimpent_class_decription { get; set; }
+
+        public bool complete { get; set; }
+    }
+}
